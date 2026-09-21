b6ad7df8d6d964cd45d6602e426c0dfe9a5d0cc4
imeqyou <67050631@kmitl.ac.th>
2026-09-21 23:33:13 +0700

    Add cell04 exercise 02

---
+++ discovery_piscine/cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first_number = float(input("Give me the first number: "))
+second_number = float(input("Give me the second number: "))
+
+print(first_number + second_number)
+print(first_number - second_number)
+print(first_number / second_number)
+print(first_number * second_number)
