8bbd62beeac8977354b45a31ec022afbdd8a377e
imeqyou <67050631@kmitl.ac.th>
2026-09-21 20:17:12 +0700

    Add cell02 exercise 03

---
+++ discovery_piscine/cell02/ex03/mult.py
@@ -0,0 +1,13 @@
+first_number = int(input("Enter the first number:\n"))
+second_number = int(input("Enter the second number:\n"))
+
+result = first_number * second_number
+
+print(f"{first_number} x {second_number} = {result}")
+
+if result > 0:
+    print("The result is positive.")
+elif result < 0:
+    print("The result is negative.")
+else:
+    print("The result is zero.")
